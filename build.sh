fbefe807e1035371b18561057d009d6de1ca428b
Sarom@BOLT <saromleang@gmail.com>
2023-07-06 15:19:15 -0500

    Update

--- build.sh
+++ build.sh
@@ -16,18 +16,22 @@ git clone https://github.com/MolSSI/QCEngine.git
 # QCElemental
 cd ${BUILDDIR}
 git clone https://github.com/MolSSI/QCElemental.git
+cd QCElemental
+git checkout v0.25.1
 
 # Special GAMESS branch for QCEngine
 cd ${BUILDDIR}
-git clone git@github.com:gms-bbg/gamess.git gamess-qcengine
-cd gamess-qcengine
-git checkout saromleang/qcengine
-./bin/create-install-info.py --gfortran --mkl --sockets --fortran_version=5.5.0 --qcengine \
- --rungms \
- --restart=${HOME}/restart \
- --scratch=${HOME}/scratch
-make -j8
-
+if [ ! -f ${BUILDDIR}/gamess-qcengine/gamess.00.x]
+then
+   git clone git@github.com:gms-bbg/gamess.git gamess-qcengine
+   cd gamess-qcengine
+   git checkout saromleang/qcengine
+   ./bin/create-install-info.py --gfortran --mkl --sockets --fortran_version=5.5.0 --qcengine \
+   --rungms \
+   --restart=${HOME}/restart \
+   --scratch=${HOME}/scratch
+   make -j8
+fi
 # PATHS
 export PYTHONPATH=${BUILDDIR}/QCEngine:${BUILDDIR}/QCElemental:${PYTHONPATH}
 export PATH=${BUILDDIR}/gamess-qcengine:${PATH}
